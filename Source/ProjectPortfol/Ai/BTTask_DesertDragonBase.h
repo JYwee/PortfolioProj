8032c1188f2487bdcfb437c910ae01adc639f641
jy_wee <toya_@live.co.kr>
2023-08-15 23:59:57 +0900

    map edit

---
+++ Source/ProjectPortfol/Ai/BTTask_DesertDragonBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ai/BTTask_NpcAiBase.h"
+#include "BTTask_DesertDragonBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPORTFOL_API UBTTask_DesertDragonBase : public UBTTask_NpcAiBase
+{
+	GENERATED_BODY()
+
+
+	
+};
