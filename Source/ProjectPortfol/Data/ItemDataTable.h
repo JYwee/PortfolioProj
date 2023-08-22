9d692aeb209a5c904ad7d01a82bfe8306c8e6faf
jy_wee <toya_@live.co.kr>
2023-08-22 23:56:34 +0900

    0822

--- Source/ProjectPortfol/Data/ItemDataTable.h
+++ Source/ProjectPortfol/Data/ItemDataTable.h
@@ -5,17 +5,17 @@
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
 #include "UObject/NoExportTypes.h"
-#include "FItemDataTable.generated.h"
+#include "ItemDataTable.generated.h"
 
 /**
  *
  */
 
-UENUM(BlueprintType)
-enum class ItemType : uint8
-{
-
-}
+//UENUM(BlueprintType)
+//enum class ItemType : uint8
+//{
+//
+//}
 
 USTRUCT(BlueprintType)
 struct PROJECTPORTFOL_API FItemDataTable : public FTableRowBase
@@ -25,6 +25,7 @@ struct PROJECTPORTFOL_API FItemDataTable : public FTableRowBase
 		FItemDataTable() {}
 	~FItemDataTable() {}
 
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
 		TSubclassOf<UObject> Object;
 };
